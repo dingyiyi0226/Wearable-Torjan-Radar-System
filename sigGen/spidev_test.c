1229f75dc97a1ccb53c89f92ba49ddd7f64751d9
EdwardLeeMacau <b05901119@g.ntu.edu.tw>
2020-02-18 13:26:12 +0800

    Modify sysfs interface

--- sigGen/spidev_test.c
+++ sigGen/spidev_test.c
@@ -21,7 +21,133 @@
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
 
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define IN  0
+#define OUT 1
+
+#define LOW  0
+#define HIGH 1
+
+#define POUT 23 /* P1-16 */
+
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+
+static int
+GPIOExport(int pin)
+{
+    #define BUFFER_MAX 3
+    char buffer[BUFFER_MAX];
+    ssize_t bytes_written;
+    int fd;
+
+    fd = open("/sys/class/gpio/export", O_WRONLY);
+
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open export for writing!\n");
+        return(-1);
+    }
+
+    bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
+    write(fd, buffer, bytes_written);
+    close(fd);
+    return(0);
+}
+
+static int
+GPIOUnexport(int pin)
+{
+    char buffer[BUFFER_MAX];
+    ssize_t bytes_written;
+    int fd;
+
+    fd = open("/sys/class/gpio/unexport", O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open unexport for writing!\n");
+        return(-1);
+    }
+
+    bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
+    write(fd, buffer, bytes_written);
+    close(fd);
+    return(0);
+}
+
+static int
+GPIODirection(int pin, int dir)
+{
+    static const char s_directions_str[]  = "in\0out";
+
+    #define DIRECTION_MAX 35
+    char path[DIRECTION_MAX];
+    int fd;
+
+    snprintf(path, DIRECTION_MAX, "/sys/class/gpio/gpio%d/direction", pin);
+    fd = open(path, O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open gpio direction for writing!\n");
+        return(-1);
+    }
+
+    if (-1 == write(fd, &s_directions_str[IN == dir ? 0 : 3], IN == dir ? 2 : 3)) {
+        fprintf(stderr, "Failed to set direction!\n");
+        return(-1);
+    }
+
+    close(fd);
+    return(0);
+}
+
+static int
+GPIORead(int pin)
+{
+    #define VALUE_MAX 30
+    char path[VALUE_MAX];
+    char value_str[3];
+    int fd;
+
+    snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
+    fd = open(path, O_RDONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open gpio value for reading!\n");
+        return(-1);
+    }
+
+    if (-1 == read(fd, value_str, 3)) {
+        fprintf(stderr, "Failed to read value!\n");
+        return(-1);
+    }
+
+    close(fd);
+
+    return(atoi(value_str));
+}
+
+static int
+GPIOWrite(int pin, int value)
+{
+    static const char s_values_str[] = "01";
+
+    char path[VALUE_MAX];
+    int fd;
+
+    snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
+    fd = open(path, O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open gpio value for writing!\n");
+        return(-1);
+    }
+
+    if (1 != write(fd, &s_values_str[LOW == value ? 0 : 1], 1)) {
+        fprintf(stderr, "Failed to write value!\n");
+        return(-1);
+    }
+
+    close(fd);
+    return(0);
+}
+
 
 static void pabort(const char *s)
 {
@@ -35,7 +161,8 @@ static uint8_t bits = 8;
 static uint32_t speed = 1e6;
 static uint16_t delay = 0;
 
-static uint8_t tx[5] = {0, };
+static uint8_t tx[5] = {0x00, 0x00, 0x08, 0x00, 0x00};
+static uint8_t tx2[5] = {0x00, 0x00, 0x80, 0x00, 0x00};
 static uint8_t rx[ARRAY_SIZE(tx)] = {0, };
 
 static int
@@ -202,9 +329,41 @@ main(int argc, char *argv[])
         .bits_per_word = bits,
     };
 
+    struct spi_ioc_transfer tr2 = {
+        .tx_buf = (unsigned long)tx2,
+        .rx_buf = (unsigned long)rx,
+        .len = ARRAY_SIZE(tx2),
+        .delay_usecs = delay,
+        .speed_hz = speed,
+        .bits_per_word = bits,
+    };
+
+    // Enable GPIO
+    if (GPIOExport(POUT) == -1)
+        return (1);
+
+    if (GPIODirection(POUT, OUT) == -1)
+        return (2);
+
     ret = 1;
 	while (ret >= 1) 
-        { ret = transfer(fd, &tr); }
+    { 
+        ret = transfer(fd, &tr); 
+        GPIOWrite(POUT, 1);
+        usleep(1e6 / speed);
+        GPIOWrite(POUT, 0);
+
+        usleep(500 * 1000);
+        
+        ret = transfer(fd, &tr2);
+        GPIOWrite(POUT, 1);
+        usleep(1e6 / speed);
+        GPIOWrite(POUT, 0);
+    }
+
+    // Disable GPIO
+    if (GPIOUnexport(POUT) == -1)
+        return (4);
 
 	close(fd);
 
