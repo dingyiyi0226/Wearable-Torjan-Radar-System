5622c4af702094931ccfe0d9a117a0415bcceb44
EdwardLeeMacau <b05901119@g.ntu.edu.tw>
2020-02-29 11:44:35 +0800

    Add ADF4158.readWord() and append comments

--- ADF4158.py
+++ ADF4158.py
@@ -13,7 +13,7 @@
   R7: DELAY REGISTER
 """
 
-import functools.wraps
+from functools import wraps
 from enum import Enum, IntEnum, unique
 import RPi.GPIO as GPIO
 
@@ -21,53 +21,80 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 # Define GPIO pins
-W_CLK  = 12
-LE     = 14
-DATA   = 18
-TXDATA = 15
-MUXOUT = 17
-RESET  = 22
+# Pin numbering scheme: 
+#   - Defined by GPIO.setmode(GPIO.BOARD)
+#   - Use physical pin number
 
-@unique
-class RampMode(IntEnum):
-    """ Write to Register 3 """
-    CONT_SAWTOOTH   = 0
-    CONT_TRIANGULAR = 1
-    SING_SAWTOOTH   = 2
-    SING_BURST      = 3
+# GND  = 6      # T3
+W_CLK  = 12     # T4
+DATA   = 16     # T5
+LE     = 18     # T6
+TXDATA = 15     # T16
+MUXOUT = 17     # T8
 
 @unique
 class Clock(IntEnum):
     FALLING_EDGE = 0
     RISING_EDGE  = 1
 
-def initGPIO():
-    for pin in (W_CLK, LE, DATA, TXDATA, RESET):
+@unique
+class RampMode(IntEnum):
+    """ DB11 - DB10 at Register 3: FUNCTION REGISTER """
+    CONT_SAWTOOTH   = 0
+    CONT_TRIANGULAR = 1
+    SING_SAWTOOTH   = 2
+    SING_BURST      = 3
+
+@unique
+class Muxout(IntEnum):
+    """ DB30 - DB27 at Register 0: FRAC/INT REGISTER """
+    THREE_STATE  = 0
+    D_VDD        = 1
+    D_GND        = 2
+    R_DIVIDER    = 3
+    N_DIVIDER    = 4
+    # RESERVED   = 5
+    DIGITAL_LOCK = 6
+    SERIAL_DATA  = 7
+    # RESERVED   = 8
+    # RESERVED   = 9
+    CLK_DIVIDER  = 10
+    # RESERVED   = 11
+    FAST_LOCK    = 12
+    R_DIVIDER_2  = 13
+    N_DIVIDER_2  = 14
+    READBACK     = 15
+
+def initGPIOPins():
+    """ GPIO Pins initialization for ADF4158 config. """
+    for pin in (W_CLK, LE, DATA):
         GPIO.setup(pin, GPIO.OUT)
     
-    for pin in (MUXOUT, ):
+    for pin in (TXDATA, MUXOUT, ):
         GPIO.setup(pin, GPIO.IN)
 
-# TODO
-def parseWord(word):
-    return
-
-# TODO
 def verbose(func):
-    @functools.wraps(func)
+    """ Decorator for sendWord(). Show words to send in binary format. """
+    @wraps(func)
     def wrapper(*args):
-        parseWord(args[0])
+        print("{}".format(bin(args[0]).zfill(32)))
         return func(*args)
 
     return wrapper
 
-
 def pulseHigh(pin):
-    """ Function to send a pulse """
+    """ 
+    Send a pulse 
+    
+    :param pin: number of pin to send a pulse
+    """
     GPIO.output(pin, True)
     GPIO.output(pin, False)
-    
-    return
+
+def setReady():
+    GPIO.output(W_CLK, False)
+    GPIO.output(DATA, False)
+    GPIO.output(LE, False)
 
 # TODO
 @verbose
@@ -78,18 +105,35 @@ def sendWord(word, clk=Clock.RISING_EDGE):
     :param clk: { Clock.RISING_EDGE, Clock.FALLING_EDGE } optional
     """
     
-    GPIO.output(LE, True)
-    
-    # Start sending bits
-    pulseHigh(TXDATA)
-
+    # Raise Clock after setup DATA 
     for i in range(31, -1, -1):
         GPIO.output(DATA, word[i])
         pulseHigh(W_CLK)
 
-    GPIO.output(LE, False)
+    # Raise LE
+    pulseHigh(LE)
+
+    # Reset Data as 0
+    GPIO.output(DATA, False)
 
     return True
+
+# TODO
+def readWord():
+    """ Readback words """
+    word = 0
+
+    GPIO.output(LE, True)
+    pulseHigh(TXDATA)
+
+    for _ in range(36, -1, -1):
+        GPIO.output(W_CLK, True)
+        word = word << 1 + GPIO.input(MUXOUT)
+        GPIO.output(W_CLK, False)
+
+    GPIO.output(LE, False)
+
+    return word
 
 # TODO
 def setModulationInterval(tm=None, fm=None):
@@ -106,13 +150,10 @@ def setDeviationRegister():
 
 # TODO
 def setStepRegister(step1, step2):
-    sendWord(0x0000A006)
-    sendWord(0x00800006)
     return
 
 # TODO
 def setDelayRegister():
-    sendWord(0x7)
     return 
 
 # TODO
@@ -139,12 +180,26 @@ def initADF4851():
         (Datasheet p.25)
     """
     
-    initGPIO()
+    initGPIOPins()
+    setReady()
+
     return True
 
-def main():
-    initADF4851()
+def test_singleFreq():
+    """ Unittest: send single freq. control words """
+    sendWord(0x00000007)
+    sendWord(0x00000006)
+    sendWord(0x00800006)
+    sendWord(0x00000005)
+    sendWord(0x00800005)
+    sendWord(0x00180104)
+    sendWord(0x00000043)
+    sendWord(0x0040800A)
+    sendWord(0x00000001)
+    sendWord(0x01220000)
 
+def test_triangle():
+    """ Unittest: send ramp freq. control words """
     sendWord(0x00000007)
     sendWord(0x0000A006)
     sendWord(0x00800006)
@@ -155,6 +210,13 @@ def main():
     sendWord(0x0740800A)
     sendWord(0x00000001)
     sendWord(0x811F8000)
+
+def main():
+    initADF4851()
+
+    while True:
+        test_singleFreq()
+        # test_triangle()
 
     return
 
