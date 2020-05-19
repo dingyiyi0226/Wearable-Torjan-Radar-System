00e282ccc276d100a2ad901ec434887f2de35d27
EdwardLeeMacau <b05901119@g.ntu.edu.tw>
2020-05-19 20:19:29 +0800

    Renamed AD-DA-Board

--- AD7770.py
+++ AD7770.py
@@ -22,7 +22,7 @@ def floor(x, step):
 def listdir(dirname):
     return sorted([os.path.join(dirname, fname) for fname in os.listdir(dirname)])
 
-def readFile(fname, ratio=1e6, removeDC=True):
+def readFile(fname, ratio=1e6, removeDC=True, **kwargs):
     df = pd.read_csv(fname, header=None, low_memory=False).loc[:, 1:8]
     df = df[df.notnull().all(axis=1)].to_numpy().transpose() / ratio
 
@@ -31,13 +31,29 @@ def readFile(fname, ratio=1e6, removeDC=True):
 
     return df
 
+def readBatchFiles(folder):
+    fnames = [fname for fname in listdir(folder) if (os.path.splitext(fname)[0] == '.csv') ]
+    print(fnames)
+
+    return
+
 def loadBG(fnames, fs, t_window, t_stride, *args, **kwargs):
     """
     Parameters
     ----------
-    fnames : list
+    fnames : {None, list}
 
     fs : float
+
+    t_window : float
+
+    t_stride : float
+
+    Return
+    ------
+    dopplerMap : np.array
+
+    spectrumAVG : np.array
     """
 
     MODULE_TIME   = kwargs['tm']
@@ -45,9 +61,11 @@ def loadBG(fnames, fs, t_window, t_stride, *args, **kwargs):
     CENTER_FREQ   = kwargs['fc']
     MODULE_POINTS = int(MODULE_TIME * fs)
 
+    waveform      = np.zeros(int(data.size))
     spectrumAVG   = np.zeros(int(data.size / 2))
     dopplerAVG    = np.zeros((data.size // MODULE_POINTS, MODULE_POINTS), dtype=np.float64)
 
+    count = 0
     for fname in fnames:
         data = readFile(fname)[0]
         data = data[:floor(data.size, MODULE_POINTS)]
@@ -60,8 +78,10 @@ def loadBG(fnames, fs, t_window, t_stride, *args, **kwargs):
             spectrumAVG += spectrum
             dopplerAVG  += doppler
 
-    spectrumAVG /= (len(fnames) * len(range(0, data.size - WINDOW_POINT, STRIDE_POINT)))
-    dopplerAVG /= (len(fnames) * len(range(0, data.size - WINDOW_POINT, STRIDE_POINT)))
+            count += 1
+
+    spectrumAVG /= count
+    dopplerAVG /= count
 
     return dopplerMap, spectrumAVG
 
@@ -73,18 +93,17 @@ def dopplerMap(m, bw, fc, tm):
 
     return dopplerMap, distance, velocity
 
-def singleFreqInference(signal, centerFreq, sampleFreq, threshold, segments, stride):
+def singleFreqInference(signal, fc, sampleFreq, threshold, segments, stride):
     SEGMENT_POINTS = int(segments * sampleFreq)
     STRIDE_POINTS  = int(stride * sampleFreq)
 
     f, t, spectrogram = stft(signal, fs=sampleFreq, nperseg=SEGMENT_POINTS, noverlap=STRIDE_POINTS)
     freqIndex = np.argmax(spectrogram, axis=0)
-    freqDiff = f[freqIndex]
-    speed = freqDiff * c / (2 * centerFreq)
-
+    speed = f[freqIndex] * c / (2 * fc)
+    
     return speed
 
-def visualize(fname, method, fs, t_window, t_stride, drop_last=True, *args, **kwargs):
+def visualize(fname, visualMethod, fs, t_window, t_stride, drop_last=True, *args, **kwargs):
     """
     Parameters
     ----------
@@ -113,7 +132,8 @@ def visualize(fname, method, fs, t_window, t_stride, drop_last=True, *args, **kw
     for start in range(0, data.size - WINDOW_POINT, STRIDE_POINT):
         # Canvas and drawing method
         for ax in axs: ax.clear()
-        method(data[start: start + WINDOW_POINT], fs, axs, **kwargs)
+        segment = data[start: start + WINDOW_POINT]
+        method(segment, fs, axs, **kwargs)
 
         # Decoration
         fig.suptitle("{}: {} - {}".format(fname, start / fs, (start + WINDOW_POINT) / fs))
@@ -127,7 +147,7 @@ def visualize(fname, method, fs, t_window, t_stride, drop_last=True, *args, **kw
 
     return
 
-def single(data, fs, axs, *args, **kwargs):
+def singleFrequency(data, fs, axs, *args, **kwargs):
     """
     Parameters
     ----------
@@ -164,7 +184,7 @@ def single(data, fs, axs, *args, **kwargs):
 
     return
 
-def triangular(data, fs, axs, *args, **kwargs):
+def singleModulation(data, fs, axs, *args, **kwargs):
     """
     Parameters
     ----------
@@ -187,8 +207,10 @@ def triangular(data, fs, axs, *args, **kwargs):
 
     MODULE_POINTS = int(MODULE_TIME * fs)
 
-    # Data Processing
+    # Data Pre-Processing
     data = data[:floor(data.size, MODULE_POINTS)]
+
+    # Data Processing
     spectrum = np.abs(fftshift(fft(data)))
     spectrum = spectrum[int(data.size / 2):]
     doppler, distance, velocity = dopplerMap(data.reshape(-1, MODULE_POINTS), BANDWIDTH, CENTER_FREQ, MODULE_TIME)
@@ -197,60 +219,6 @@ def triangular(data, fs, axs, *args, **kwargs):
     freqAxis = np.linspace(0, fs / 2, spectrum.shape[0], endpoint=False)
     maxIndex = np.argmax(spectrum)
 
-    # Doppler Map
-    ax = axs[0]
-    img = NonUniformImage(ax, cmap=cm.Purples)
-    img.set_data(distance, velocity, doppler)
-    ax.images.append(img)
-    ax.set_xlim(np.min(distance), np.max(distance))
-    ax.set_ylim(np.min(velocity), np.max(velocity))
-    ax.set_title('Doppler Map')
-
-    # Time Axis
-    ax = axs[1]
-    ax.plot(timeAxis, data)
-    ax.set_title('Waveform')
-
-    # Frequency Axis
-    ax = axs[2]
-    ax.plot(freqAxis, spectrum)
-    ax.annotate("{:.2f}".format(freqAxis[maxIndex]), (freqAxis[maxIndex], np.max(spectrum)))
-    ax.set_title('Frequency')
-
-    return
-
-def sawtooth(data, fs, axs, *args, **kwargs):
-    """
-    Parameters
-    ----------
-    data : np.array
-
-    fs : float
-        sampling frequency
-
-    t_window : float
-        observed time
-
-    t_stride : float
-        sliding length of time
-    """
-    # Constant
-    MODULE_TIME   = kwargs['tm']
-    BANDWIDTH     = kwargs['bw']
-    CENTER_FREQ   = kwargs['fc']
-
-    MODULE_POINTS = int(MODULE_TIME * fs)
-
-    # Data Processing
-    data = data[:floor(data.size, MODULE_POINTS)]
-    spectrum = np.abs(fftshift(fft(data)))
-    spectrum = spectrum[int(data.size / 2):]
-    doppler, distance, velocity = dopplerMap(data.reshape(-1, MODULE_POINTS), BANDWIDTH, CENTER_FREQ, MODULE_TIME)
-
-    timeAxis = np.linspace(0, data.shape[0] / fs, data.shape[0], endpoint=False)
-    freqAxis = np.linspace(0, fs / 2, spectrum.shape[0], endpoint=False)
-    maxIndex = np.argmax(spectrum)
- 
     # Doppler Map
     ax = axs[0]
     img = NonUniformImage(ax, cmap=cm.Purples)
@@ -276,8 +244,11 @@ def sawtooth(data, fs, axs, *args, **kwargs):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--dir', help="Directory of the records.")
-    parser.add_argument('-r', '--removeBG', nargs='*', help="Remove background if background specified.")
+    parser.add_argument('-r', '--removeBG', nargs='*', help="Remove background if background file is specified.")
     parser.add_argument('-c', '--channel', type=int, default=1, help="Read from selected AD7770 Channel")
+    parser.add_argument('--bandwidth', type=float, default=1e8, help="Bandwidth of the Radar")
+    parser.add_argument('--centerFreq', type=float, default=5.8e9, help="Center frequency of the Radar")
+    parser.add_argument('--moduleTime', type=float, default=[4e-3], nargs='*', help="Interval of Frequency Modulation.")
     parser.add_argument('--compressor', default=1.0, type=float, help="Help visualizing doppler map if lower than 1.0")
     parser.add_argument('--repeat', action='store_true', help="Repeat until KeyboardInterrupt if set true.")
     parser.add_argument('--stride', default=0.125, type=float, help="Specified sliding length of each step")
@@ -287,38 +258,54 @@ def main():
     # CONSTANT
     SAMPLE_FREQ = 1.6e4
     SAMPLE_TIME = 1 / SAMPLE_FREQ
-    BANDWIDTH   = 1e8
-    CENTER_FREQ = 5.8e9
-    MODULE_TIME = 4e-3
-    
+
+    BANDWIDTH    = args.bandwidth
+    CENTER_FREQ  = args.centerFreq
+    MODULE_TIME  = args.moduleTime[0]    
     SEGMENT_TIME = args.window
     STRIDE_TIME  = args.stride
 
-    # bgFiles = [fname for fname in listdir('./rawdata/0503_triangular') if os.path.basename(fname).startswith('00')]
-    # loadBG(bgFiles, SAMPLE_FREQ, SEGMENT_TIME, STRIDE_TIME, fc=CENTER_FREQ, bw=BANDWIDTH, tm=MODULE_TIME)
+    # Selecting Background
+    if isinstance(args.removeBG, list):
+        args.removeBG = [os.path.join(args.dir, fname) for fname in args.removeBG]
 
-    # for fname in listdir('./rawdata/0503_single'):
-    #     visualize(fname, single, SAMPLE_FREQ, SEGMENT_TIME, STRIDE_TIME, fc=CENTER_FREQ)
+        fig, axs = plt.subplots(nrows=len(args.removeBG) + 1, ncols=3, figsize=(19.2, 10.8))
+        timeAxis = np.arange(0, int(args.window * SAMPLE_FREQ)) / SAMPLE_FREQ
+        freqAxis = np.linspace(-SAMPLE_FREQ / 2, SAMPLE_FREQ / 2, int(args.window * SAMPLE_FREQ), endpoint=False)
+        spectrums = np.zeros((len(args.removeBG), int(args.window * SAMPLE_FREQ)))
 
-    # for fname in listdir('./rawdata/0503_triangular'):
-    #     visualize(fname, triangular, SAMPLE_FREQ, SEGMENT_TIME, STRIDE_TIME, fc=CENTER_FREQ, bw=BANDWIDTH, tm=2 * MODULE_TIME)
+        for index, fname in enumerate(args.removeBG):
+            data = readFile(fname)[args.channel, :int(args.window * SAMPLE_FREQ)]
+            spectrumBG = np.abs(fftshift(fft(data))) / data.size
+            spectrums[index] = spectrumBG
+            axs[index, 0].plot(timeAxis, data)
+            axs[index, 1].plot(freqAxis, spectrumBG)
+            axs[index, 1].set_xlim(0, np.max(freqAxis))
+            axs[index, 1].semilogy()
+            axs[index, 0].set_title('Waveform: {}'.format(fname))
+            axs[index, 1].set_title('Spectrum: {}'.format(fname))
 
-    # for fname in listdir('./rawdata/0503_sawtooth'):
-    #     visualize(fname, sawtooth, SAMPLE_FREQ, SEGMENT_TIME, STRIDE_TIME, fc=CENTER_FREQ, bw=BANDWIDTH, tm=MODULE_TIME)
+        for index, fname in enumerate(args.removeBG):
+            axs[index, 2].plot(freqAxis, np.log(spectrums[index]) - np.log(np.mean(spectrums, axis=0)))
+            axs[index, 2].semilogy()
+            axs[index, 2].set_xlim(0, np.max(freqAxis))
+            axs[index, 2].set_title('Reject BG: {}'.format(fname))
 
-    # for fname in listdir('./rawdata/0505_single'):
-    #     visualize(fname, single, SAMPLE_FREQ, SEGMENT_TIME, STRIDE_TIME, fc=CENTER_FREQ)
+        data = readFile(os.path.join(args.dir, '200101.csv'))[1, :int(args.window * SAMPLE_FREQ)]
+        axs[len(args.removeBG), 0].plot(timeAxis, data)
+        axs[len(args.removeBG), 1].plot(freqAxis, np.abs(fftshift(fft(data))) / data.size)
+        axs[len(args.removeBG), 2].plot(freqAxis, np.abs(fftshift(fft(data))) / data.size - np.mean(spectrums, axis=0))        
+        axs[len(args.removeBG), 1].semilogy()
+        axs[len(args.removeBG), 2].semilogy()
+        axs[len(args.removeBG), 2].set_xlim(0, np.max(freqAxis))
 
-    # for fname in listdir('./rawdata/0505_triangular'):
-    #     visualize(fname, triangular, SAMPLE_FREQ, SEGMENT_TIME, STRIDE_TIME, fc=CENTER_FREQ, bw=BANDWIDTH, tm=2 * MODULE_TIME)
+        plt.savefig('bg.png')
+
+    raise
 
     # -------------------------------------------------------- # 
     # Experiment 0512                                          #
     # -------------------------------------------------------- #
-
-    # filenames = [fname for fname in listdir('./rawdata/0512_sawtooth_distance') if os.path.basename(fname).endswith('1.csv')]
-    # for fname in filenames:
-    #     visualize(fname, sawtooth, SAMPLE_FREQ, SEGMENT_TIME, STRIDE_TIME, fc=CENTER_FREQ, bw=BANDWIDTH, tm=MODULE_TIME)
 
     filenames = [fname for fname in listdir('./rawdata/0512_sawtooth_4ms') if os.path.basename(fname).endswith('1.csv')]
     for fname in filenames:
@@ -333,6 +320,12 @@ def main():
     # filenames = [fname for fname in listdir('./rawdata/0512_sawtooth_16ms') if os.path.basename(fname).endswith('1.csv')]
     # for fname in filenames:
     #     visualize(fname, sawtooth, SAMPLE_FREQ, SEGMENT_TIME, STRIDE_TIME, fc=CENTER_FREQ, bw=BANDWIDTH, tm=MODULE_TIME)
+
+    # -------------------------------------------------------- # 
+    # Experiment 0514                                          #
+    # -------------------------------------------------------- #
+
+
 
     return
 
