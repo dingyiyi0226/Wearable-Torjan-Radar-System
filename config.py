7d3a32a2fd8a90683d0e5a858753b9dbbdabeae4
EdwardLeeMacau <b05901119@g.ntu.edu.tw>
2020-06-19 17:48:44 +0800

    Added config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+ADF_HIGH_PINS = {
+    # 'GND': 6,        # T3
+    'W_CLK': 12,       # T4
+    'DATA' : 16,       # T5
+    'LE'   : 18,       # T6
+    'TXDATA' : 13,     # T16
+    'MUXOUT' : 15,     # T8
+}
+
+ADF_LOW_PINS = {
+    # 'GND': 60,        # T3
+    'W_CLK': 120,       # T4
+    'DATA' : 160,       # T5
+    'LE'   : 180,       # T6
+    'TXDATA' : 130,     # T16
+    'MUXOUT' : 150,     # T8
+}
+
+DIR_PINS = {
+    'STEP': 16,
+    'DIR' : 18,
+    'ENA' : 7,  # no use
+}
